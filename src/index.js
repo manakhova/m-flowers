252c7ef3c6faa23f2043ab297a5e0728b055cdcc
Tatiana Manakhova <tat.manakhova@gmail.com>
2022-11-25 22:09:13 +0300

    feature: tablet markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import './scss/main.scss'; 
+import "./scss/main.scss";
 
+const header = document.querySelector(".header");
 
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+
+  scrollY > 40
+    ? header.classList.add("scrolled")
+    : header.classList.remove("scrolled");
+});
