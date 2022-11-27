a01f0e0ce852cf4d7443218778dd925c4fe72abb
Tatiana Manakhova <tat.manakhova@gmail.com>
2022-11-27 22:08:41 +0300

    fix: pixel perfect markup

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ window.addEventListener("scroll", () => {
     ? header.classList.add("scrolled")
     : header.classList.remove("scrolled");
 });
+
+const nextButton = document.querySelector(".start__button--next");
+const prevButton = document.querySelector(".start__button--prev");
+const firstStep = document.querySelector("#first-step");
+const secondStep = document.querySelector("#second-step");
+
+nextButton.addEventListener('click', () => {
+  firstStep.style.display = 'none';
+  secondStep.style.display = "flex";
+});
+prevButton.addEventListener("click", () => {
+  firstStep.style.display = "flex";
+  secondStep.style.display = "none";
+});
